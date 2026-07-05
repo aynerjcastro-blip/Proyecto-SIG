0ff0113e25fdb5f6d05fa4192838a50a7731d4d9
kstrodeveloper <aynerjosec@gmail.com>
2026-07-05 10:27:35 -0500

    Agregar la estructura inicial del proyecto con la configuración de Maven y Spring Boot.
    - Crear los archivos .gitattributes y .gitignore para la configuración del proyecto.
    - Agregar archivos wrapper de Maven para facilitar la gestión de Maven.
    - Inicializar el archivo pom.xml con las dependencias y la configuración de Spring Boot.
    - Implementar la clase principal de la aplicación para Spring Boot.
    - Agregar propiedades de la aplicación para el nombre de la aplicación.
    - Crear una clase de prueba para la carga del contexto de la aplicación.

---
+++ src/main/java/com/kstrodev09/sig_obras_backend/SigObrasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kstrodev09.sig_obras_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigObrasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigObrasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kstrodev09/sig_obras_backend/SigObrasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kstrodev09.sig_obras_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigObrasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
