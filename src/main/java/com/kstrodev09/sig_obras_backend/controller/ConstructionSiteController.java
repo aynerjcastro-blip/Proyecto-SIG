e2f798afcb2aa9fce711059d0fb242b675d40eae
kstrodeveloper <aynerjosec@gmail.com>
2026-07-30 17:19:30 -0500

    feat(controller): add CRUD operations and delete endpoint to ConstructionSiteController

--- src/main/java/com/kstrodev09/sig_obras_backend/controller/ConstructionSiteController.java
+++ src/main/java/com/kstrodev09/sig_obras_backend/controller/ConstructionSiteController.java
@@ -2,7 +2,8 @@ package com.kstrodev09.sig_obras_backend.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,21 +16,23 @@ import com.kstrodev09.sig_obras_backend.entity.ConstructionSiteState;
 import com.kstrodev09.sig_obras_backend.entity.StatusHistory;
 import com.kstrodev09.sig_obras_backend.service.ConstructionSiteService;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @RestController
 @RequestMapping("/api")
 public class ConstructionSiteController {
-    
+
     private final ConstructionSiteService constructionSiteService;
 
     public ConstructionSiteController(ConstructionSiteService constructionSiteService) {
         this.constructionSiteService = constructionSiteService;
     }
-    
 
     @PutMapping("/construction-sites/{id}/status")
     public ConstructionSite updateStatus(@PathVariable Long id, @RequestBody ConstructionSiteState newState) {
         return constructionSiteService.updateState(id, newState);
-        
+
     }
 
     @GetMapping("/construction-sites/{id}/history")
@@ -37,5 +40,26 @@ public class ConstructionSiteController {
         return constructionSiteService.findHistory(id);
     }
 
-    
+    @GetMapping("/construction-sites")
+    public List<ConstructionSite> findAll() {
+        return constructionSiteService.findAll();
+    }
+
+    @GetMapping("/consttruction-sites/{id}")
+    public ConstructionSite findById(@PathVariable Long id) {
+        return constructionSiteService.findById(id);
+    }
+
+    @PostMapping("/construction-sites")
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public ConstructionSite create(@RequestBody ConstructionSite constructionSite) {
+        return constructionSiteService.save(constructionSite);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        constructionSiteService.deleteById(id);
+    }
 }
